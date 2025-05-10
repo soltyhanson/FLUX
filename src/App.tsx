312fee67c2e15f96e97547a0f9ca7679d51e9135
soltyhanson <soltyhanson@gmail.com>
2025-05-10 20:30:56 +0800

    Update Login component to remove loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -8,47 +8,42 @@ import AdminDashboard from './pages/dashboard/AdminDashboard';
 import ClientDashboard from './pages/dashboard/ClientDashboard';
 import TechDashboard from './pages/dashboard/TechDashboard';
 
-const App: React.FC = () => {
-  return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          {/* Public routes */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+const App: React.FC = () => (
+  <AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
 
-          {/* Protected dashboards */}
-          <Route 
-            path="/dashboard/admin" 
-            element={
-              <ProtectedRoute roles={['admin']}>
-                <AdminDashboard />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/dashboard/client" 
-            element={
-              <ProtectedRoute roles={['client']}>
-                <ClientDashboard />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/dashboard/technician" 
-            element={
-              <ProtectedRoute roles={['technician']}>
-                <TechDashboard />
-              </ProtectedRoute>
-            } 
-          />
+        <Route
+          path="/dashboard/admin"
+          element={
+            <ProtectedRoute roles={['admin']}>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/client"
+          element={
+            <ProtectedRoute roles={['client']}>
+              <ClientDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/technician"
+          element={
+            <ProtectedRoute roles={['technician']}>
+              <TechDashboard />
+            </ProtectedRoute>
+          }
+        />
 
-          {/* Redirect everything else to login */}
-          <Route path="*" element={<Navigate to="/login" replace />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
-  );
-};
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
+  </AuthProvider>
+);
 
 export default App;
