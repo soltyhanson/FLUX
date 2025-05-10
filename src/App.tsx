da44ae75a21b9364493d9d3e3d5d7edd5ff80002
soltyhanson <soltyhanson@gmail.com>
2025-05-10 21:40:39 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,58 @@
+// src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import ProtectedRoute from './components/ProtectedRoute';
+import Layout from './components/Layout';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import AdminDashboard from './pages/dashboard/AdminDashboard';
 import ClientDashboard from './pages/dashboard/ClientDashboard';
 import TechDashboard from './pages/dashboard/TechDashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 
-const App: React.FC = () => (
-  <AuthProvider>
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
 
-        <Route
-          path="/dashboard/admin"
-          element={
-            <ProtectedRoute roles={['admin']}>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard/client"
-          element={
-            <ProtectedRoute roles={['client']}>
-              <ClientDashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard/technician"
-          element={
-            <ProtectedRoute roles={['technician']}>
-              <TechDashboard />
-            </ProtectedRoute>
-          }
-        />
+            <Route
+              path="/dashboard/admin"
+              element={
+                <ProtectedRoute role="admin">
+                  <AdminDashboard />
+                </ProtectedRoute>
+              }
+            />
 
-        <Route path="*" element={<Navigate to="/login" replace />} />
-      </Routes>
-    </Router>
-  </AuthProvider>
-);
+            <Route
+              path="/dashboard/client"
+              element={
+                <ProtectedRoute role="client">
+                  <ClientDashboard />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/dashboard/technician"
+              element={
+                <ProtectedRoute role="technician">
+                  <TechDashboard />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route path="*" element={<Navigate to="/login" replace />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </AuthProvider>
+  );
+}
 
 export default App;
+
